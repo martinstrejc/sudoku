588bce85f965faee221f5296823c95118ce85d4f
strma17 <martin.strejc@ca.com>
2016-12-26 23:13:07 +0100

    Full Cell populator

--- src/main/java/cz/wicketstuff/sudoku/web/page/SudokuHomePage.java
+++ src/main/java/cz/wicketstuff/sudoku/web/page/SudokuHomePage.java
@@ -3,9 +3,17 @@
  */
 package cz.wicketstuff.sudoku.web.page;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.DataGridView;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
  * @author Martin Strejc
@@ -21,25 +29,59 @@ public class SudokuHomePage extends WebPage
 	 */
 	public SudokuHomePage()
 	{
-		// TODO Auto-generated constructor stub
+		List<ICellPopulator<ArrayList<Integer>>> columns = new ArrayList<>(9);
+		columns.add(new ICellPopulator<ArrayList<Integer>>()
+		{
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void detach()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void populateItem(Item<ICellPopulator<ArrayList<Integer>>> cellItem,
+				String componentId, IModel<ArrayList<Integer>> rowModel)
+			{
+				cellItem.add(new Label(componentId, () -> rowModel.getObject().get(1)));
+			}
+		});
+		 
+		queue(new WebMarkupContainer("table"));
+		queue(new DataGridView<ArrayList<Integer>>("rows", columns, new ListDataProvider<ArrayList<Integer>>(generateData())));
+	}
+	
+	public static List<ArrayList<Integer>> generateData() {
+		
+		final ArrayList<Integer> rowVector = new ArrayList<>(9);
+		rowVector.add(1);
+		rowVector.add(2);
+		rowVector.add(3);
+		rowVector.add(4);
+		rowVector.add(5);
+		rowVector.add(6);
+		rowVector.add(7);
+		rowVector.add(8);
+		rowVector.add(9);
+		
+		final List<ArrayList<Integer>> rows = new ArrayList<>(9);
+		rows.add(rowVector);
+		rows.add(rowVector);
+		rows.add(rowVector);
+
+		rows.add(rowVector);
+		rows.add(rowVector);
+		rows.add(rowVector);
+
+		rows.add(rowVector);
+		rows.add(rowVector);
+		rows.add(rowVector);
+
+		return rows;
 	}
 
-	/**
-	 * @param model
-	 */
-	public SudokuHomePage(IModel<?> model)
-	{
-		super(model);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param parameters
-	 */
-	public SudokuHomePage(PageParameters parameters)
-	{
-		super(parameters);
-		// TODO Auto-generated constructor stub
-	}
 
 }
